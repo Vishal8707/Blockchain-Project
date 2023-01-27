aa23851c8fc8430709b362cba0bbb07b6f4838ef
Vishal8707 <360vishalsharma.mech@gmail.com>
2023-01-27 14:20:12 +0530

    project

--- src/Controllers/coinController.js
+++ src/Controllers/coinController.js
@@ -1,26 +1,29 @@
-const coinModel = require('../models/coinModel')
-
+const coinModel = require("../models/coinModel");
 
 const coin = async function (req, res) {
-    // let db = await coinModel.find().select("symbol name marketCapUsd priceUsd -_id")
-    // if (db.length == 0) {
-    const response = await fetch('https://api.coincap.io/v2/assets', {
-        headers: {
-            Authorization: `Bearer ${process.env.API_KEY}`
-        }
-    })
-    const { data } = await response.json()
-    const sort = data.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr)      //.map((coin)=>{return {symbol:coin.symbol,name:coin.name,marketCapUsd:coin.marketCapUsd,priceUsd:coin.priceUsd}})
-    let newData = sort.map(({ symbol, name, marketCapUsd, priceUsd }) => { return { symbol, name, marketCapUsd, priceUsd } })
-    let resData = []
-    newData.forEach(async (coin) => {
-        let data = await coinModel.findOneAndUpdate({ symbol: coin.symbol }, coin, { new: true, upsert: true })
-        resData.push(data)
-    })
-    let db = await coinModel.find().select("symbol name marketCapUsd priceUsd -_id")
-    // }
-    res.send({ data: db })
-}
+  const response = await fetch("https://api.coincap.io/v2/assets", {
+    headers: {
+      Authorization: `Bearer ${process.env.API_KEY}`,
+    },
+  });
+  const { data } = await response.json();
+  const sort = data.sort((a, b) => b.changePercent24Hr - a.changePercent24Hr);
+  let newData = sort.map(({ symbol, name, marketCapUsd, priceUsd }) => {
+    return { symbol, name, marketCapUsd, priceUsd };
+  });
+  let resData = [];
+  newData.forEach(async (coin) => {
+    let data = await coinModel.findOneAndUpdate({ symbol: coin.symbol }, coin, {
+      new: true,
+      upsert: true,
+    });
+    resData.push(data);
+  });
+  let db = await coinModel
+    .find()
+    .select("symbol name marketCapUsd priceUsd -_id");
+  // }
+  res.send({ data: db });
+};
 
-
-module.exports = { coin }
+module.exports = { coin };
